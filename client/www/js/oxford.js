4de89f7e4712faaa71966d3c25a366c444b09cdc
kulinacs <kulinacs@bellicosedev.com>
2016-03-27 01:28:29 -0500

    UNFINISHED

--- client/www/js/oxford.js
+++ client/www/js/oxford.js
@@ -1,3 +1,20 @@
-function upload(b64photo) {
-    
-}
+var oxfordAPI = '9f3c1b463d4e462aa615b0ceb6a5dd95';
+var oxfordURL = 'https://api.projectoxford.ai/emotion/v1.0/recognize';
+
+function upload(imageData) {
+    var uploadData = atob(imageData);
+    $.ajax({
+	url: oxfordURL,
+	type: 'post',
+	data: {
+	    uploadData,
+	},
+	headers: {
+	    Ocp-Apim-Subscription-Key: oxfordAPI,
+	},
+	dataType: 'octet-stream',
+	success: function (data) {
+	    console.log(data);
+	}
+    });
+};
