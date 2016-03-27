a2b7d531f04033f4268c5d56c832a5bef165add0
Sam G <thegladiusmaximus@gmail.com>
2016-03-27 04:56:58 -0500

    added text to emotion api

--- client/platforms/browser/www/js/index.js
+++ client/platforms/browser/www/js/index.js
@@ -16,9 +16,24 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var ToneAnalyzer = {
+	"url": "https://gateway.watsonplatform.net/tone-analyzer-beta/api/v3/tone?version=2016-02-11",
+    "password": "TFYRJbMsuwUD",
+    "username": "f2793151-c1a6-44f4-92d8-a1cb8bf96412"
+};
+
 var app = {
     // Application Constructor
-    initialize: function() { },
+    initialize: function() {
+		this.faceData = _.mapValues(WatsonToToneEnum, function() {
+			// https://lodash.com/docs#mapValues
+			return 0; // use the original key, set the value to zero.
+		});
+		this.toneData = _.mapValues(WatsonToToneEnum, function() {
+			// https://lodash.com/docs#mapValues
+			return 0; // use the original key, set the value to zero.
+		});
+	},
 
 	handlePictureSnap: function(imageData) {
 		document.getElementById('camera').setAttribute('src', "data:image/jpeg;base64," + imageData);
@@ -32,4 +47,31 @@ var app = {
 		navigator.camera.getPicture(app.handlePictureSnap, app.onFail, { quality: 50 });
 	},
     
+	textToEmotion: function(input, callback) {
+		var input = "All our homes are lost our loved ones taken by the sea.";
+		$.ajax({
+			type: 'POST',
+			url: ToneAnalyzer.url,
+
+			contentType: 'application/json',
+			data: JSON.stringify({'text': input}),
+			username: ToneAnalyzer.username,
+			password: ToneAnalyzer.password,
+			success: function(data) {
+				var tone_categories = data['document_tone']['tone_categories'];
+				_.forEach(tone_categories, function(tone_category) {
+					if (tone_category['category_id'] === 'emotion_tone') {
+						var output = _.map(tone_category['tones'], function(tone_category) {
+							var result = {};
+							result[WatsonToToneEnum[tone_category['tone_id']]] = tone_category['score'];
+							return result;
+						});
+						callback(output);
+						return;
+					}
+				});
+			},
+		});
+	},
+
 };
